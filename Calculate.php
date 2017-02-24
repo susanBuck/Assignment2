c79d3b24323cf4359fc8a6560ba622063817508f
chenShen <Lazyts@users.noreply.github.com>
2017-02-23 22:14:43 -0500

    Add files via upload

---
+++ Calculate.php
@@ -0,0 +1,22 @@
+<?php
+
+require('Form.php');
+
+use Assignment2\Form;
+$form = new Form($GET);
+
+#values
+class Calculate{
+  $value=$form->get('value','');
+  $rent=$form->get('rent','');
+  $months = $form->isChosen('month','');
+  $otherincome = $form->isChosen('otherincome','');
+  $expense = $form->isChosen('$expense','');
+
+  public function__calculate($value,$rent,$months,$otherincome,$expense)
+  {
+    $caprate=(($rent*$months+$otherincome)-$expense)/$value;
+    echo 'The CapRate for this property is: $caprate';
+  }
+
+?>#eoc
