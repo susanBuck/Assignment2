27343b8d6eb5daad2ead4559c28c4943c6783c2d
Susan Buck <susan@susanbuck.net>
2017-03-10 02:38:41 -0500

    Update Calculate.php

--- Calculate.php
+++ Calculate.php
@@ -1,24 +1,16 @@
 <?php
 
-require('Form.php');
+require('form.php');
+use DWA\Form;
 
-use Assignment2\Form;
 $form = new Form($_GET);
 
-#values
-class Calculate{
-  $value=$form->get('value','');
-  $rent=$form->get('rent','');
-  $months = $form->isChosen('month','');
-  $otherincome = $form->get('otherincome','');
-  $expense = $form->get('$expense','');
+$value = $form->get('value','');
+$rent = $form->get('rent','');
+$months = $form->get('months','');
+$otherincome = $form->get('otherincome','');
+$expense = $form->get('expense','');
 
-    if($form->isSubmitted()){
-      public function__calculate($value,$rent,$months,$otherincome,$expense)
-      {
-        $caprate=(($rent*$months+$otherincome)-$expense)/$value;
-        echo 'The CapRate for this property is: $caprate';
-      }
-    }
-  }
-?>#end of class
+if($form->isSubmitted()){
+    $caprate = (($rent*$months+$otherincome)-$expense)/$value;
+}
